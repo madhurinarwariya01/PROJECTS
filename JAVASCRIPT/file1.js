1166cbc4eb7c4d8be6fc41555c52fd0d070ec697
madhuri narwariya <narwariyamadhuri9@gmail.com>
2024-09-25 20:12:54 +0530

    Add files via upload

---
+++ JAVASCRIPT/file1.js
@@ -0,0 +1,329 @@
+// document.write("welcome to js")
+// document.write("welcome <br>  js")
+// const profile = {
+//     name : "madhuri",
+//     post: 20,
+//     follower: "2K",
+//     following : 205
+
+// }
+
+
+// console.log("pofile")
+
+// const dis = {
+//     distance : 250,
+//     average: 10,
+//     fulecost : 106,
+//     totaldistance : "distance/avg*fulecost;
+// }
+
+
+// let a = 20;
+// b = 10;
+
+// console.log("a - b ");
+
+// let a = 2
+// b = 3
+
+
+// console.log("a**b")
+
+// let a = 5
+// a--
+// console.log("a--")
+
+// let a = 2 
+// a += 5;
+// console.log("a")
+
+// let a = 2;
+// b=2;
+// console.log("a==b")
+
+// let a = 7;
+// b = 7;
+// console.log("a>5 && b>6")
+
+// let 
+// a = 3;
+// b = 4;
+// console.log("a+b")
+
+// let
+// s = 3;
+// a = s * s;
+//  console.log("a")
+ 
+// let
+// a = 2;
+// b = 4;
+// c = 6;
+// avg = a+b+c/3;
+// console.log("avg")
+
+
+// let
+// a = 2 
+// b = 6
+// console.log("a>=b")
+
+
+// let
+// eng = 20;
+// hin = 30;
+// maths = 45;
+//   per = eng + hin + maths/ 150*100
+//   console.log("per")
+
+// let
+// age = a
+// b = 18
+
+// console.log("a>=b" you are )
+
+// let age = 15;
+// if(age>=18)
+// {document.write("you are adult")}
+// else
+// {document.write("you are child")}
+
+// let principal = 6000;
+// rate = 2;
+// time = 3;
+// si = principal*rate*time/100;
+//  {console.log("simpale intrest:", si)}
+
+// let principal = 5000;
+// rate = 3;
+
+
+// time = 4;
+// amt = principal*(Math.pow((1+rate/100),time));
+// ci = amt-principal;
+//  console.log("compount interest:", ci);
+
+// let num = 6;
+
+// if(num%2==0)
+// {
+//     console.log("number is even:" , num );
+// }
+// else
+// {console.log("number is odd:", num);}
+
+
+// let age = 10;
+// if(age<18)
+// {
+//     document.write("you are elegible for vote:", age);
+
+// }
+// else(age>18)
+// {
+//     document.write("you are not elegible for vote:", age);
+// }
+
+
+
+// let maths = 70;
+// phy = 70;
+// chem = 60;
+// total_sub =maths+ phy+ chem;
+
+// if(maths>=65 && phy>=55 && chem>=50 && total_sub>=190)
+
+
+// {
+//     document.write("you are elegible for this course" );
+
+// }
+// else
+// {
+//     document.write("you are not elegible for this course");
+
+// }
+
+// let temp =40;
+// if(temp<0)
+// {
+//     document.write(" freezing temprature");
+
+// }
+// else if(temp>20)
+// {
+//     document.write("temperature is normal");
+// }
+
+
+// else if(temp>30)
+// {
+//     document.write("temparature is hot")
+// }
+
+
+// let
+// eng = 45;
+// hindi = 59;
+// maths = 64;
+// phy = 74;
+// chem = 74;
+// total_marks = eng +hindi+ maths+ phy+ chem;
+// per = total_marks/500*100;
+// if(per>=60)
+// document.write("1st division");
+// else if(per>=50)
+//     document.write("2nd division");
+// else if(per>=40)
+//     document.write("3rd division");
+//  if(per>=80)
+//     document.write(" A grade"); 
+// else if(per>=70)
+//     document.write(" B grade");
+// else if (per>=60)
+//     document.write(" c grade")
+
+
+// let alphabet = 'a';
+// if(alphabet=='a' || alphabet=='e' || alphabet=='i' || alphabet =='o' || alphabet=='u')
+// document.write("alphabet");
+//     else
+//     {document.write("constant");}
+
+// let
+// name = akash;
+// unit = 300;
+
+// if(unit>100)
+//     document.write("charge"free);
+// else if(unit>200)
+//     document.write("chrge"+ unit*)
+
+
+    // let 
+    // months = 6;
+
+    // switch(months)
+    // {
+   
+    // case 1:
+    //     document.write("JANUARY")
+    //     break;
+    //  case 2:
+    //      document.write("FEBURARY")
+    //      break;
+    //  case 3:
+    //       document.write("MARCH")
+    //       break;
+    // case 4:
+    //      document.write("APRIL")
+    //      break;
+    // case 5:
+    //      document.write("MAY")
+    //      break;
+    // case 6:
+    //       document.write("JUNE")
+    //       break;
+    //  case 7:     
+    //       document.write("JULY")
+    //       break;
+    //       case 8:     
+    //       document.write("AUGUST")
+    //       break;
+    //       case 9:     
+    //       document.write("SEPTEMBER")
+    //       break;
+    //       case 10:     
+    //       document.write("OCTOBER")
+    //       break;
+    //       case 11:     
+    //       document.write("NOVEMBER")
+    //       break;
+    //       case 12:     
+    //       document.write("DECEMBER")
+    //       break;
+          
+         
+          
+
+
+    // }
+
+    // let
+    // age = 8;
+    //  let result = (age>=18 ? "adult" : "child")
+    //   document.write("you are :" , result)
+
+
+// let amt = 10000;
+// if()
+//     document.write("your bill is :", totalbill);
+// else
+// document.write(bill);
+
+/// LOOPS IN JS ////
+
+// for(let i = 1;  i<=100 ; i++)
+//     if(i%2==0)
+//         document.write("even number =" , i+"<br>")
+//     else
+//     document.write( i ,"= odd number  " , )
+// // {
+//     document.write(i+"<br>")
+// }
+
+let num = prompt("ENTER");
+//  sum = 0;
+for(let i=1 ; i<=10; i++)
+    
+      {
+        sum = num*i;
+       document.write (num ," x ",i ," = ",num*i , "<br>");
+      }
+
+    
+// //     }
+
+
+// let num = prompt("ENTER");
+// sum = 0;
+// f
+
+
+// let i = 1;
+
+// do{
+//     document.write(i);
+//     i++;
+
+// }
+// while(i<10)
+
+
+//    for( let i=1; i<=100; i++)
+//    {
+// if(i==25)
+// {
+//     document.write("value found",i, "<br>");
+//     continue;
+// }
+// document.write(i , "<br>")
+// }
+
+// for(let name = 1; name<=20; name++)
+// {
+//     document.write("madhui narwaiya", )
+// }
+
+// for(let i = 1; i<=10; i++)
+// {
+//     // document.write(i, "<br>")
+//     {
+//       for(let b = 1; b<=i; b++)
+//         document.write(i)
+
+//     }
+    
+//     document.write("<br>")
+// }
